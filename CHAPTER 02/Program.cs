359862a8117b6bb7a3b156e58d8bc6ab78e9f3d6
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-12 17:45:33 +0530

    Add project files.

---
+++ CHAPTER 02/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 2.Swap values of two integer variables without using a third variable ?
+
+int a = 20;
+int b = 300;
+
+Console.WriteLine("before swapping");
+Console.WriteLine("a = " + b);
+Console.WriteLine("b = " + b);
+
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+
+Console.WriteLine("after swapping");
+Console.WriteLine("a = " + b);
+Console.WriteLine("b = " + b);
